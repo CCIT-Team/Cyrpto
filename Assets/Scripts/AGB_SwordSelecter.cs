5d63d27333758d40dae50e3f5d55ad4d1ca531cd
GrandChickenMaster <agb3711@gmail.com>
2022-03-31 12:40:01 +0900

    First commit

---
+++ Assets/Scripts/AGB_SwordSelecter.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AGB_SwordSelecter : MonoBehaviour
+{
+    GameObject SwordObject;
+    GameObject Asword;
+    GameObject Bsword;
+
+
+    private void Awake()
+    {
+        SwordObject = this.transform.gameObject;
+    }
+    private void Start()
+    {
+        Asword = SwordObject.transform.GetChild(0).gameObject;
+        Bsword = SwordObject.transform.GetChild(1).gameObject;
+        Asword.SetActive(true);
+        Bsword.SetActive(false);
+
+
+    }
+
+    public void SelectSword(bool triger)
+    {
+        if (!triger)
+        {
+            Asword.SetActive(true);
+            Bsword.SetActive(false);
+        }
+        else
+        {
+            Asword.SetActive(false);
+            Bsword.SetActive(true);
+        }
+
+    }
+}
