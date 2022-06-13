169bcde0f5c6c540cc45ccf6dc704cfc76a250e7
MoccaHazel <101385960+MoccaHazel@users.noreply.github.com>
2022-06-13 15:41:40 +0900

    map1 추가

---
+++ Assets/Scripts/Map/MapCreate.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapCreate : MonoBehaviour
+{
+    public GameObject[] map_Part;
+    int partCount;
+    List<GameObject> ActiveMaps = new List<GameObject>();
+    GameObject delete;
+    int activeCount = 0;
+    int ran;
+    // Start is called before the first frame update
+    void Start()
+    {
+        partCount = map_Part.Length -1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ran = Random.Range(0, partCount);
+        switch (activeCount)
+        {
+            case 0:
+                ActiveMaps.Add(Instantiate(map_Part[ran]));
+                ActiveMaps[0].transform.position = this.gameObject.transform.position;
+                activeCount++;
+                break;
+            case 1:
+                ActiveMaps.Add(Instantiate(map_Part[ran]));
+                ActiveMaps[1].transform.position = ActiveMaps[0].transform.position + new Vector3(0, 0, 99);
+                activeCount++;
+                break;
+            case 2:
+                ActiveMaps.Add(Instantiate(map_Part[ran]));
+                ActiveMaps[2].transform.position = ActiveMaps[1].transform.position + new Vector3(0, 0, 99);
+                activeCount++;
+                break;
+            case 3:
+                ActiveMaps.Add(Instantiate(map_Part[ran]));
+                ActiveMaps[3].transform.position = ActiveMaps[2].transform.position + new Vector3(0, 0, 99);
+                activeCount++;
+                break;
+            case 4:
+                break;
+        }
+
+        if (ActiveMaps[0].transform.position.z <= -100)
+        {
+            delete = ActiveMaps[0];
+            ActiveMaps.Remove(ActiveMaps[0]);
+            Destroy(delete);
+            activeCount--;
+        }
+    }
+}
---
+++ Assets/Scripts/Map/MapMoving.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapMoving : MonoBehaviour
+{
+    public float moveSpeed = 0.5f;
+    int rot;
+    // Update is called once per frame
+    void Start()
+    {
+        rot = (int)transform.rotation.y / 4;
+    }
+
+    void Update()
+    {
+        switch (rot)
+        {
+            case 0:
+                transform.Translate(new Vector3(0, 0, -moveSpeed));
+                break;
+            case 1:
+                transform.Translate(new Vector3(-moveSpeed, 0, 0));
+                break;
+            case 2:
+                transform.Translate(new Vector3(0, 0, moveSpeed));
+                break;
+            case 3:
+                transform.Translate(new Vector3(moveSpeed, 0, 0));
+                break;
+        }
+    }
+}
