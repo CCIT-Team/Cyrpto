bd2120e63e9b4f434d588fd1b76d8bac46f1bfb6
DAEYEONLIM <dy990320@naver.com>
2022-06-20 18:56:01 +0900

    Gamemanager

--- Assets/Scripts/HpSetter.cs
+++ Assets/Scripts/HpSetter.cs
@@ -8,7 +8,7 @@ public class HpSetter : MonoBehaviour
     void Start()
     {
         GameManager.Instance.maxHp = 100;
-
+        GameManager.Instance.hp = 0;
     }
 
     // Update is called once per frame
