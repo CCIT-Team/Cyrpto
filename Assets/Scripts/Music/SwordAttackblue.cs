6277688d52662ca835f3be202092c784126bb54b
tuost <fldh753@gmail.com>
2022-06-23 18:22:55 +0900

    구닥다리 ui

--- Assets/Scripts/Music/SwordAttackblue.cs
+++ Assets/Scripts/Music/SwordAttackblue.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SwordAttackblue : MonoBehaviour
+public class SwordAttackblue:MonoBehaviour
 {
     public static int Combo = 0;
     void OnTriggerEnter(Collider col)
     {
-        if (col.tag == "Enemy_closs" || col.tag == "Enemy_far")
+        if (col.tag == "BlueEnemy")
         {
             Destroy(col.gameObject);
             AttackPlayer.a = true;
